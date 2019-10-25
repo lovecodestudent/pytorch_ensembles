6cce5b4956a135b24142aedf2a2020cc8de10a32
Prathamesh Mandke <mandkepk97@gmail.com>
2019-10-25 14:35:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Ensembling PyTorch models
+
+## Steps
+
+* Create an Ensemble object from src/ensemble.py
+
+```
+from src.ensemble import Ensemble
+
+my_ensemble = Ensemble('/path/to/models/', ensemble_type='avg')
+
+input = torch.randn(1, 3, 224, 224) # input tensor image -> (batch_size, channels, height, weight)
+
+prediction = my_ensemble.predict_ensemble(input)
+
+# prediction stores the index of the predicted class.
+
+```
+
+## Author
+
+[Prathamesh Mandke](mailto:pkmandke@vt.edu).
---
+++ src/ensemble.py
@@ -0,0 +1,75 @@
+'''
+
+Ensembling ImageNet models for streaming DL applications.
+
+Author: Prathamesh Mandke
+
+Date created: 10/25/2019
+
+'''
+
+import os
+import time
+from datetime import timedelta
+
+import torch
+
+
+class Ensemble:
+
+    def __init__(self, model_path, ensemble_type='avg'):
+        ''' Initialize an Ensemble object.
+
+        model_path: path to a directory that contains the models to be ensembled. Ensure that the path contains nothing else other than the models.
+
+        ensemble_type: 'avg' is the only one supported right now. It averages th results ffrom all ensembles.
+
+        '''
+
+        self.ensemble_type = ensemble_type;
+
+        self.model_path = model_path
+
+        self.ensemble_score = None
+
+
+    def get_prediction(self, input, model, path=False):
+        '''path: True if model variable passed above is a path instead the model instance'''
+        if path:
+            try:
+                if self.model_path[-1] == '/':
+                    model = torch.load(self.model_path + _)
+                else:
+                    model = torch.load(self.model_path + '/' + _)
+            except:
+                print("error reading {}".format(_))
+
+        return model(input)
+
+    def predict_ensemble(self, input):
+
+        self.num_ensembles = 0.0
+
+        for _ in os.listdir(self.model_path):
+
+            torch.cuda.empty_cache()
+
+            print("Forward propagating {}...".format(_))
+            t1 = time.monotonic()
+            try:
+                if self.model_path[-1] == '/':
+                    model = torch.load(self.model_path + _)
+                else:
+                    model = torch.load(self.model_path + '/' + _)
+            except:
+                print("error reading {}".format(_))
+                continue
+
+            self.ensemble_score += self.get_prediction(input, model)
+
+            self.num_ensembles += 1.0
+
+            print("Time taken = {}s".format(timedelta(seconds=time.monotonic() - t1)))
+
+        if self.ensemble_type == 'avg':
+            return torch.max(self.ensemble_score/self.num_ensembles, 1)[1].item()
